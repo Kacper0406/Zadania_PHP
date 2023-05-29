512d6f6269c9ccdc41446b86a97db028b63ce4da
2ir02 <z.kacper2006@gmail.com>
2023-05-29 09:13:31 +0200

    Initial commit

---
+++ Zadanie 00/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Moja strona</title>
+</head>
+<body>
+    <h1>Witaj na mojej stronie</h1>
+    <hr/>
+
+    <?php
+        echo "<h2><b>Test</b></h2>"
+    ?>
+</body>
+</html>
---
+++ Zadanie 01/z01.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <h1>Zadanie 01</h1>
+    <h2>Autor: Kacper Zając</h2>
+    <hr>
+</header>
+<section>
+    <p>Program wczytuje imię i wyświetla powitanie z tym imieniem</p>
+    <form action="z01w.php" method="post">
+        <label for="imie">Podaj imię:</label>
+        <input type="text" id="imie" name="imie">
+        <input type="submit" value="Wyślij">
+    </form>
+</section>
+</body>
+</html>
